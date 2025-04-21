a7a39db12506cff880814671ed1e39059cd87626
Leo Vogel <schweiz.vogel@gmail.com>
2025-04-21 22:28:47 +0200

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,34 +3,22 @@ import './Login.css';
 import {
   GoogleAuthProvider,
   getRedirectResult,
-  onAuthStateChanged,
   signInWithPopup,
-  signInWithRedirect,
-  signOut,
 } from 'firebase/auth';
-import { useEffect, useState } from 'react';
 
 import { addUser } from '../../firebase/useFireBaseUsers';
 import { auth } from '../../firebase/firebase';
-import { isMobile } from '../../helpers/isMobile';
+import { useEffect } from 'react';
 
 const provider = new GoogleAuthProvider();
 
 const Login = () => {
-  const [currentUser, setCurrentUser] = useState(null);
   const user = auth.currentUser;
 
   const handleLogin = async () => {
     console.log('Starting login...');
 
     try {
-      // Redirect the user for authentication
-
-      // if (isMobile()) {
-      //   const { user } = await signInWithRedirect(auth, provider);
-      //   return;
-      // }
-      // await signInWithRedirect(auth, provider);
       await signInWithPopup(auth, provider);
       addUser();
     } catch (error) {
@@ -38,28 +26,12 @@ const Login = () => {
     }
   };
 
-  const handleLogout = async () => {
-    await signOut(auth);
-  };
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-    });
-    return () => unsubscribe();
-  }, []);
-
   useEffect(() => {
     const fetch = async () => {
       const response = await getRedirectResult(auth);
       console.log(response);
     };
     fetch();
-
-    //  await addUser({
-    //    name: newUser.displayName,
-    //    email: newUser.email,
-    //    contactPhone: newUser.phoneNumber,
-    //  });
   }, []);
 
   return (
