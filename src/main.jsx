d9dcd264963b7082a19b363150bff2561ae9652d
Leo Vogel <schweiz.vogel@gmail.com>
2025-05-09 14:59:15 +0200

    Changed to use HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import './index.css';
 
 import App from './App.jsx';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { UserProvider } from './context/UserContext.jsx';
 import { createRoot } from 'react-dom/client';
